115cf7af029b31fe7f7f0d7872a6352f0cf7ecb9
Fedeito <federicohermo@gmail.com>
2021-08-01 23:32:34 -0300

    inicio del proyecto

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql= MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='sistema2123'
+mysql.init_app(app)
