e48abc467aa64fe9c8a2b4017e953a149e25a2b3
Fedeito <federicohermo@gmail.com>
2021-08-02 00:37:07 -0300

    creacion app e index

--- app.py
+++ app.py
@@ -4,9 +4,22 @@ from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-mysql= MySQL()
+mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='sistema2123'
 mysql.init_app(app)
+
+@app.route('/')
+def index():
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'laura', 'obama@ciudad.com.ar', 'obamaura.jpg'); "
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
+    return render_template('empleados/index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
