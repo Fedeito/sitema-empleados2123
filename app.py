52b4be85769e679d1f8234a4dd1bcecfd4a58433
Fedeito <federicohermo@gmail.com>
2021-08-03 15:27:17 -0300

    sda

--- app.py
+++ app.py
@@ -34,6 +34,11 @@ def index():
 def destroy(id):
     conn = mysql.connect()
     cursor = conn.cursor()
+
+    cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+    fila = cursor.fetchall()
+    os.remove(os.path.join(app.config["CARPETA"], fila[0][0]))
+
     cursor.execute("DELETE FROM empleados WHERE id=%s", (id))
     conn.commit()
 
