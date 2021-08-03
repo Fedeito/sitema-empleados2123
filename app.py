00b8081020ddac7f58d48a30fdac4f39479ba082
Fedeito <federicohermo@gmail.com>
2021-08-03 12:17:24 -0300

    insercion sin foto

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-#MODIFICACION
-# Mas ComodificacionesM
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
@@ -13,15 +11,37 @@ app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='sistema2123'
 mysql.init_app(app)
 
-@app.route('/')
-def index():
-    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'laura', 'obama@ciudad.com.ar', 'obamaura.jpg'); "
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    conn.commit()
+# EJEMPLO HARDCODEADO
+# @app.route('/')
+# def index():
+#     sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'messi', 'messi@ciudad.com.ar', 'messi.jpg'); "
+#     conn = mysql.connect()
+#     cursor = conn.cursor()
+#     cursor.execute(sql)
+#     conn.commit()
+#     return render_template('empleados/index.html')
 
-    return render_template('empleados/index.html')
+@app.route('/create')
+def create():
+   return render_template('empleados/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+   _nombre = request.form['txtNombre']
+   _correo = request.form['txtCorreo']
+   _foto   = request.files['txtFoto']
+
+   sql   = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s); "
+   datos = (_nombre, _correo, _foto.filename)
+
+   conn = mysql.connect()
+   cursor = conn.cursor()
+   cursor.execute(sql, datos)
+   conn.commit()
+   # mysql.connect().cursor().execute(sql, datos)
+   # mysql.connect().commit()
+
+   return render_template('empleados/index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
